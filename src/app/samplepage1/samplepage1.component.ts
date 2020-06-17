5cdd67a81d6aebbe901b1d6658f3f76ee857ea93
unknown <geethadevarajan2403@gmail.com>
2020-06-17 10:06:20 +0530

    myproject

---
+++ src/app/samplepage1/samplepage1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-samplepage1',
+  templateUrl: './samplepage1.component.html',
+  styleUrls: ['./samplepage1.component.css']
+})
+export class Samplepage1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
